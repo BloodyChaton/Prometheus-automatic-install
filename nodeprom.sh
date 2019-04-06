a820bc8d6c7df31fce40f5e7988a32728f5ebfe3
Luc <martinon.luc@gmail.com>
2019-04-06 03:40:31 -0700

    Install and configure node Exporter / user prometheus

---
+++ nodeprom.sh
@@ -0,0 +1,20 @@
+su - prometheus -c "wget https://github.com/prometheus/node_exporter/releases/download/v0.16.0-rc.1/node_exporter-0.16.0-rc.1.linux-amd64.tar.gz"
+su - prometheus -c "tar -xzvf node_exporter-0.16.0-rc.1.linux-amd64.tar.gz"
+su - prometheus -c "mv node_exporter-0.16.0-rc.1.linux-amd64 node_exporter"
+cd /etc/systemd/system/
+touch node_exporter.service
+echo "[Unit]
+Description=Node Exporter
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=prometheus
+ExecStart=/home/prometheus/node_exporter/node_exporter
+
+[Install]
+WantedBy=default.target" > node_exporter.service
+
+systemctl daemon-reload
+systemctl start node_exporter
+systemctl enable node_exporter 
