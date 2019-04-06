e6fe1917a353cada2bad2a6c42cffecb73ba7c78
Luc <martinon.luc@gmail.com>
2019-04-06 06:55:27 -0700

    Removed the configuration of prometheus

--- nodeprom.sh
+++ nodeprom.sh
@@ -1,9 +1,10 @@
 su - prometheus -c "wget https://github.com/prometheus/node_exporter/releases/download/v0.16.0-rc.1/node_exporter-0.16.0-rc.1.linux-amd64.tar.gz"
 su - prometheus -c "tar -xzvf node_exporter-0.16.0-rc.1.linux-amd64.tar.gz"
-su - prometheus -c "mv node_exporter-0.16.0-rc.1.linux-amd64 node_exporter"
+su - prometheus -c "mv node_exporter-0.16.0-rc.1.linux-amd64 /home/prometheus/node_exporter"
 cd /etc/systemd/system/
 touch node_exporter.service
-echo "[Unit]
+echo "
+[Unit]
 Description=Node Exporter
 Wants=network-online.target
 After=network-online.target
@@ -18,8 +19,3 @@ WantedBy=default.target" > node_exporter.service
 systemctl daemon-reload
 systemctl start node_exporter
 systemctl enable node_exporter
-
-echo " - job_name: 'node_exporter'
-    static_configs:
-      - targets: ['localhost:9100']" >> /home/prometheus/prometheus/prometheus.yml
-systemctl restart prometheus 
